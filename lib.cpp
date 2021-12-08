e3a670b950acc44869bcfe11fd76a0ff0b887c87
EdoardoCavallino <91631725+EdoardoCavallino@users.noreply.github.com>
2021-12-08 15:41:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include "lib.h"
+
+float maggiore(int dim, float lista[]){
+    float m = lista [0];
+    for(int i=0; i < dim ; i++){
+        if( lista[i] >= m ){
+            m = lista[i];
+        }
+    }
+    return m;
+}
+
